4366b84cf63892530a848b602f9cc874c52ecd95
Marcus Holanda <mvsh777@hotmail.com>
2023-01-14 21:43:23 -0300

    acao de esconder o navegador

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 import configparser
 from pathlib import Path
 import sys
-from dateutil.parser import parse
+
 
 ARQ_INIT = Path() / "config.init"
 VERSION_CLI = 'HUNGER RUSH VERSION CLI 0.1'
@@ -31,10 +31,11 @@ if __name__ == '__main__':
     parser.add_argument('-v', '--version', action="version")
     parser.add_argument('-d', '--date', help="initial date", type=str, required=True)
     parser.add_argument('-p', '--period', help="execution period", type=int)
+    parser.add_argument('-i', '--invisible', help="show or hide browser", type=bool)
 
     args = parser.parse_args()
 
     if args.date:
         dias = args.period if args.period else 1
-
-        main_spider(args.date, dias, login, password, url)
+        invisible = args.invisible if args.invisible else False
+        main_spider(args.date, dias, login, password, url, invisible)
