4e3a43d377a7ba715d032f3f31c75ab547f15568
Marcus Holanda <mvsh777@hotmail.com>
2023-10-11 15:07:26 -0300

    retirado o web driver do relatorio spider

--- spider/spider.py
+++ spider/spider.py
@@ -1,8 +1,6 @@
-from webdriver_manager.firefox import GeckoDriverManager
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
